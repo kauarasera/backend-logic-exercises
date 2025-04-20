e2df5150f477295419a1a1cad21d2b8a94ef6eda
bino1 <r.asera@outlook.com>
2025-04-20 13:53:13 +0100

    feat(switch): implement user option menu using text blocks and switch expressions

---
+++ exercicios/src/week_1/exercises/conditionals/OptionMenuSwitch.java
@@ -0,0 +1,52 @@
+package week_1.exercises.conditionals;
+
+import java.util.Scanner;
+
+/*
+ 1. O que preciso fazer? -> Criar um programa que mostre 3 opcoes:
+    1 - Cadastrar usuário
+    2 - Deletar usuário
+    3 - Atualizar usuário
+    O Usuario digita o numero da opcao e o programa deve exibir uma mensagem
+    correspondendo a acao escolhida. Se for digitado errado exibir uma mensagem invalida
+
+ 2. Que tipo de dados estou lidando? -> int
+
+ 3. Ferramentas que vamos usar no Java:
+    Scanner para ler a entrada do usuário
+    Variáveis do tipo int
+    Operaçao com switch
+    default para opcao invalida
+    System.out.println()
+
+  4.Passos em pseudocódigo:
+    Criar o Scanner
+    entrada com as opcoes do menu
+    Usar switch para executar a lógica de classificacao
+    Exibir ou resultado com System.out.println()
+ */
+
+public class OptionMenuSwitch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("""
+                Escolha uma opção:
+                
+                1 - Cadastrar usuário
+                2 - Deletar usuário
+                3 - Atualizar usuário
+                """);
+
+        int opcao = scanner.nextInt();
+
+        switch (opcao) {
+            case 1 -> System.out.println("Cadastrando o usuário");
+            case 2 -> System.out.println("Deletando o usuário");
+            case 3 -> System.out.println("Atualizando usuário");
+            default -> System.out.println("Opcao invalida");
+        }
+
+        scanner.close();
+    }
+}

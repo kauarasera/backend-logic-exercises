4f9642ea37514833bb29e743e94f6a2a643fd170
bino1 <r.asera@outlook.com>
2025-04-27 10:57:19 +0100

    feat(for, switch): implement divisible numbers counter and Multiple validator with menu options

---
+++ exercicios/src/week_1/exercises/loops/DivisibleCounter.java
@@ -0,0 +1,47 @@
+package week_1.exercises.loops;
+
+import java.util.Scanner;
+
+public class DivisibleCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the limit number for your counter: ");
+        int loops = scanner.nextInt();
+
+        System.out.println("""
+                Choose an option:
+                1 - Show numbers divisible by 2 and 3
+                2 - Show numbers divisible only by 2
+                3 - Show numbers divisible only by 3
+                """);
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1 -> {
+                for (int i = 1; i <= loops; i++) {
+                    if (i % 2 == 0 && i % 3 == 0) {
+                        System.out.println(i);
+                    }
+                }
+            }
+            case 2 -> {
+                for (int i = 1; i <= loops; i++) {
+                    if (i % 2 == 0 && i % 3 != 0) {
+                        System.out.println(i);
+                    }
+                }
+            }
+            case 3 -> {
+                for (int i = 1; i <= loops; i++) {
+                    if (i % 3 == 0 && i % 2 != 0) {
+                        System.out.println(i);
+                    }
+                }
+            }
+            default -> System.out.println("❌ Invalid Option");
+        }
+
+        scanner.close();
+    }
+}
--- exercicios/src/week_1/exercises/loops/MultipleValidatorApp.java
+++ exercicios/src/week_1/exercises/loops/MultipleValidatorApp.java
@@ -32,7 +32,7 @@ public class MultipleValidatorApp {
                 for (int i = 1; i <= number; i++) {
                     if (i % 3 == 0) {
                         System.out.println(i);
-                    }
+                     }
                 }
             }
             case 3 -> {
